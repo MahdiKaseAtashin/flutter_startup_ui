0a08675d593dc07194e1a602c9d246d39c6d8a4a
mahdi <MahdiKaseAtashin@gmail.com>
2022-01-13 21:01:00 +0330

    implement sample startup with package concentric_transition

--- startup_ui/lib/main.dart
+++ startup_ui/lib/main.dart
@@ -1,115 +1,182 @@
 import 'package:flutter/material.dart';
+import 'package:concentric_transition/concentric_transition.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(OnboardingExample());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class PageData {
+  final String? title;
+  final IconData? icon;
+  final Color bgColor;
+  final Color textColor;
 
-  // This widget is the root of your application.
+  PageData({
+    this.title,
+    this.icon,
+    this.bgColor = Colors.white,
+    this.textColor = Colors.black,
+  });
+}
+
+class OnboardingExample extends StatelessWidget {
+  final List<PageData> pages = [
+    PageData(
+      icon: Icons.format_size,
+      title: "Choose your\ninterests",
+      textColor: Colors.white,
+      bgColor: Color(0xFFFDBFDD),
+    ),
+    PageData(
+      icon: Icons.hdr_weak,
+      title: "Drag and\ndrop to move",
+      bgColor: Color(0xFFFFFFFF),
+    ),
+    PageData(
+      icon: Icons.bubble_chart,
+      title: "Local news\nstories",
+      bgColor: Color(0xFF0043D0),
+      textColor: Colors.white,
+    ),
+  ];
+
+  List<Color> get colors => pages.map((p) => p.bgColor).toList();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: ConcentricPageView(
+          colors: colors,
+//          opacityFactor: 1.0,
+//          scaleFactor: 0.0,
+          radius: 30,
+          curve: Curves.ease,
+          duration: Duration(seconds: 2),
+//          verticalPosition: 0.7,
+//          direction: Axis.vertical,
+//          itemCount: pages.length,
+//          physics: NeverScrollableScrollPhysics(),
+          itemBuilder: (index, value) {
+            PageData page = pages[index % pages.length];
+            // For example scale or transform some widget by [value] param
+            //            double scale = (1 - (value.abs() * 0.4)).clamp(0.0, 1.0);
+            return Container(
+              child: Theme(
+                data: ThemeData(
+                  textTheme: TextTheme(
+                    headline6: TextStyle(
+                      color: page.textColor,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: 'Helvetica',
+                      letterSpacing: 0.0,
+                      fontSize: 17,
+                    ),
+                    subtitle2: TextStyle(
+                      color: page.textColor,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                child: PageCard(page: page),
+              ),
+            );
+          },
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class PageCard extends StatelessWidget {
+  final PageData page;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  const PageCard({
+    Key? key,
+    required this.page,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: 30.0,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      child: Column(
+//        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _buildPicture(context),
+          SizedBox(height: 30),
+          _buildText(context),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _buildText(BuildContext context) {
+    return Text(
+      page.title!,
+      style: Theme.of(context).textTheme.headline6,
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Widget _buildPicture(
+    BuildContext context, {
+    double size = 190,
+    double iconSize = 170,
+  }) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(60.0)),
+        color: page.bgColor
+//            .withBlue(page.bgColor.blue - 40)
+            .withGreen(page.bgColor.green + 20)
+            .withRed(page.bgColor.red - 100)
+            .withAlpha(90),
+      ),
+      margin: EdgeInsets.only(
+        top: 140,
+      ),
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Positioned.fill(
+            child: RotatedBox(
+              quarterTurns: 2,
+              child: Icon(
+                page.icon,
+                size: iconSize + 20,
+                color: page.bgColor
+                    .withBlue(page.bgColor.blue - 10)
+                    .withGreen(220),
+              ),
+            ),
+            right: -5,
+            bottom: -5,
+          ),
+          Positioned.fill(
+            child: RotatedBox(
+              quarterTurns: 5,
+              child: Icon(
+                page.icon,
+                size: iconSize + 20,
+                color: page.bgColor.withGreen(66).withRed(77),
+              ),
+            ),
+          ),
+          Icon(
+            page.icon,
+            size: iconSize,
+            color: page.bgColor.withRed(111).withGreen(220),
+          ),
+        ],
+      ),
     );
   }
 }
